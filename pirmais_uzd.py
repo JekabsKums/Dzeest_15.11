72cf1c41d1159f9b79f39e6aecae855d892f7f46
JekabsKums <jekabs.kums@grobinasskola.lv>
2023-11-15 10:50:22 +0200

    pirmais_uzd

---
+++ pirmais_uzd.py
@@ -0,0 +1,8 @@
+"""
+Izveidot programmu, kura prasa lietotājam ievadīt divus skaitļus
+ un aprēķina to summu izvadot termināli
+"""
+a=int(input("Ievadi pirmo skaitli"))
+b=int(input("Ievadi otro skaitli"))
+c=a+b
+print(f'a+b={c}')
